d9850fd6041d6bab7a4270d74e2883a066c12674
qi xu <qi.xu@carrier.com>
2022-02-17 21:25:14 +0100

    add hello function

--- greeting/main.go
+++ greeting/main.go
@@ -8,3 +8,7 @@ import (
 func HelloQi() {
 	fmt.Printf("Hello, Qi!  Today is %s \n", time.Now().Format("2006-January-02"))
 }
+
+func Hello(name string) {
+	fmt.Printf("Hello, %s !  Today is %s \n", name, time.Now().Format("2006-January-02"))
+}
