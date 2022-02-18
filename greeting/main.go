bec380d1043ea226aa817eefefddc91e962c2175
qi xu <qi.xu@carrier.com>
2022-02-18 21:13:05 +0100

    added foreword statement

--- greeting/main.go
+++ greeting/main.go
@@ -5,10 +5,17 @@ import (
 	"time"
 )
 
+//HelloQi greeting to Qi
 func HelloQi() {
 	fmt.Printf("Hello, Qi!  Today is %s \n", time.Now().Format("2006-January-02"))
 }
 
+//Hello greeting to (string)
 func Hello(name string) {
 	fmt.Printf("Hello, %s !  Today is %s \n", name, time.Now().Format("2006-January-02"))
+}
+
+//Foreword statement (string)
+func Foreword(words string) {
+	fmt.Printf("%s ! \n  Today is %s \n", words, time.Now().Format("2006-January-02"))
 }
